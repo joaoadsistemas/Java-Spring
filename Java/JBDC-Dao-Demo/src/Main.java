d7e33dc1fc3c5f79169ff2b14aa107aa2f3abc84
Joao Henrique <joaoadsistemas@gmail.com>
2023-10-04 19:52:14 -0300

    findById implementation

--- Java/JBDC-Dao-Demo/src/Main.java
+++ Java/JBDC-Dao-Demo/src/Main.java
@@ -1,9 +1,17 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
 
 
 
