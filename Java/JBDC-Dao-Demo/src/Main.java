812b6ce6efd4e66dad7b8235e743c07fff318596
Joao Henrique <joaoadsistemas@gmail.com>
2023-10-04 20:26:54 -0300

    findByDepartment implementation

--- Java/JBDC-Dao-Demo/src/Main.java
+++ Java/JBDC-Dao-Demo/src/Main.java
@@ -1,6 +1,9 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -13,6 +16,15 @@ public class Main {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
+        System.out.println();
+
+        System.out.println("=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj: list
+             ) {
+            System.out.println(obj);
+        }
 
 
     }
