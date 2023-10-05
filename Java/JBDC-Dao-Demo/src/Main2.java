c43a73d719025c0340f9db321e38b12fd18ad983
Joao Henrique <joaoadsistemas@gmail.com>
2023-10-04 21:17:53 -0300

    Init DepartmentDao implementation

---
+++ Java/JBDC-Dao-Demo/src/Main2.java
@@ -0,0 +1,21 @@
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main2 {
+    public static void main(String[] args) {
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDAo();
+
+        
+
+    }
+}
