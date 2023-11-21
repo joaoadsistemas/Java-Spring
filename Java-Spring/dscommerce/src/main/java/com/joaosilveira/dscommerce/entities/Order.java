4f5348d03aefa6c9a0407e5d30e68bfa5496e117
Joao Henrique <joaoadsistemas@gmail.com>
2023-11-21 14:04:22 -0300

    Order and User ManyToOne

---
+++ Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/entities/Order.java
@@ -0,0 +1,85 @@
+package com.joaosilveira.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // deixa o horario como UTC
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+
+    // joinColumn é usado para dizer qual vai ser o nome da chave estrangeira
+    // lado muitos para um com Usuarios
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+
+    public Order() {
+
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.joaosilveira.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+
+}
