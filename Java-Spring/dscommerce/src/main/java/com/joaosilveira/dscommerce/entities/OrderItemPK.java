7545a5dc0104962fdc0a5e038f46d970ade66bbd
Joao Henrique <joaoadsistemas@gmail.com>
2023-11-22 15:31:53 -0300

    HashCode and equals

--- Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/entities/OrderItemPK.java
+++ Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/entities/OrderItemPK.java
@@ -5,6 +5,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class OrderItemPK implements Serializable {
@@ -34,5 +35,18 @@ public class OrderItemPK implements Serializable {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
     }
 }
