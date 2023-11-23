e9b7a5395652628c3786ee4d8092bd4dafbf628a
Joao Henrique <joaoadsistemas@gmail.com>
2023-11-23 20:24:22 -0300

    GET Products paged

--- Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/controllers/ProductController.java
+++ Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/controllers/ProductController.java
@@ -3,10 +3,14 @@ package com.joaosilveira.dscommerce.controllers;
 import com.joaosilveira.dscommerce.dto.ProductDTO;
 import com.joaosilveira.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -18,6 +22,12 @@ public class ProductController {
     @GetMapping("/{id}")
     public ProductDTO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
+    }
+
+    // Pageable tem que ser do spring.domain
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
 
--- Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/services/ProductService.java
+++ Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.joaosilveira.dscommerce.dto.ProductDTO;
 import com.joaosilveira.dscommerce.entities.Product;
 import com.joaosilveira.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +26,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> product = repository.findAll(pageable);
+        return product.map(ProductDTO::new);
     }
 
 
