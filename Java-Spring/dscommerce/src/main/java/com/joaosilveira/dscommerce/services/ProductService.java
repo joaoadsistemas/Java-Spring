a30cb3a5afaafe6b749bb39fa55124ea7e95ae24
Joao Henrique <joaoadsistemas@gmail.com>
2023-11-23 20:37:31 -0300

    POST Insert Product

--- Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/services/ProductService.java
+++ Java-Spring/dscommerce/src/main/java/com/joaosilveira/dscommerce/services/ProductService.java
@@ -33,5 +33,19 @@ public class ProductService {
         return product.map(ProductDTO::new);
     }
 
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+
 
 }
