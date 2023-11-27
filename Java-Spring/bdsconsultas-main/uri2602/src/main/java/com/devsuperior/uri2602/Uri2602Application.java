91ea5e2218f3ab0dc11df9b762162c87a008f690
Joao Henrique <joaoadsistemas@gmail.com>
2023-11-27 14:21:44 -0300

    jpql spring basic select

--- Java-Spring/bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ Java-Spring/bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -27,9 +27,18 @@ public class Uri2602Application implements CommandLineRunner {
 		List<CustomerMinProjection> list = repository.search1("RS");
 		List<CustomerMinDTO> result1 = list.stream().map(CustomerMinDTO::new).collect(Collectors.toList());
 
+		System.out.println("\n*** RESULTADO SQL RAIZ:");
 		for (CustomerMinDTO obj : result1) {
 			System.out.println(obj);
 		}
 
+		System.out.println("\n\n");
+
+		List<CustomerMinDTO> result2 = repository.search2("RS");
+
+		System.out.println("\n*** RESULTADO JPQL:");
+		for (CustomerMinDTO obj : result2) {
+			System.out.println(obj);
+		}
 	}
 }
--- Java-Spring/bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ Java-Spring/bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2602.repositories;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projections.CustomerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +11,13 @@ import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-
+    // sql
     @Query(nativeQuery = true, value = "SELECT name FROM customers WHERE UPPER(state)  = UPPER(:state)")
     List<CustomerMinProjection> search1(String state);
+
+    // jpql
+    @Query("SELECT new com.devsuperior.uri2602.dto.CustomerMinDTO(obj.name) FROM Customer obj WHERE UPPER(obj.state) " +
+            "= UPPER(:state) ")
+    List<CustomerMinDTO> search2(String state);
 
 }
