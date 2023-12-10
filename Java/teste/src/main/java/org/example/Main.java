8e0851fa2690c259aee66c2438a6cad7040ae08c
Joao Henrique <joaoadsistemas@gmail.com>
2023-12-10 18:55:10 -0300

    .

---
+++ Java/teste/src/main/java/org/example/Main.java
@@ -0,0 +1,96 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // INT = 1, 2, 457
+        // DOUBLE = 2.35, 4.2, 5.4
+        // STRING = "2123, JDSJ ASKD 2J1"
+        // BOOLEAN = TRUE, FALSE
+
+//
+//        int idade = 17;
+//        String nome= "mariana";
+//        double salario = 50000.89;
+
+       // System.out.println("meu nome é " + nome + " e minha idade é "+ idade);
+
+
+
+        // TRABLAHO NA CLINICA SAO JOSÉ DEPOIS DA
+        // OBERNARDES
+
+//        String trabalho = "clinica sao jose";
+//        String escola= "bernardes";
+//        System.out.println("trabalho na " + trabalho + " depois do " + escola);
+
+        // SE TIVER SOL VOU A PRAIA, SE NAO VOU FICAR EM CASA
+        //
+
+        // if / else
+//        boolean mariVaiTrabalhar = true;
+
+//        if (mariVaiTrabalhar == true) {
+//            System.out.println("trabalho na " + trabalho + " depois do " + escola);
+//        } else {
+//            System.out.println("A MARI NAO FOI TRABALHAR POR QUE É DOMINGO");
+//        }
+//
+//
+//        idade = 17;
+//
+//        if (idade >= 18) {
+//            System.out.println("A PESSOA É MAIOR DE IDADE");
+//        } else {
+//            System.out.println("A PESSOA É MENOR DE IDADE");
+//        }
+
+
+
+        // tipos de dados;
+        // estrutra condicional
+        // lista
+        // estrutura repetitiva
+
+
+//        List<String> frutas = new ArrayList<>();
+//        frutas.add("Morango");
+//        frutas.add("Banana");
+//        frutas.add("Maça");
+//        frutas.add("Pera");
+//        frutas.add("Uva");
+//
+////        System.out.println(frutas);
+//
+//        for (String fruta: frutas) {
+//            System.out.println("O NOME DA FRUTA É " + fruta);
+//        }
+
+//        String nome = "Mariana";
+//        int idade = 17;
+//
+//        String nome2 = "Joao";
+//        int idade2 = 20;
+//
+//        String nome3 = "kleber";
+//        int idade3 = 40;
+//
+//        System.out.println(soma(30,30));
+//
+        int a = -54;
+        int b = 98;
+        int c = a + b;
+        System.out.println(c);
+//
+}
+//
+//    public static Double soma(double n1, double n2) {
+//        return n1 + n2;
+//    }
+
+}
+
