949b4250738f54cf96556f0d8ce11fb9ef06f047
Joao Henrique <joaoadsistemas@gmail.com>
2023-12-26 13:44:25 -0300

    Project Setup

---
+++ Java-Spring/challenge-auth-security/src/main/java/com/joaosilveira/challengeauthsecurity/ChallengeAuthSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.joaosilveira.challengeauthsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAuthSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeAuthSecurityApplication.class, args);
+    }
+
+}
---
+++ Java-Spring/challenge-auth-security/src/test/java/com/joaosilveira/challengeauthsecurity/ChallengeAuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaosilveira.challengeauthsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAuthSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
