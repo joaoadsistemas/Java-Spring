6eb2dd2e65834464a3b18caa494343d434ff7449
Joao Henrique <joaoadsistemas@gmail.com>
2024-01-21 14:03:28 -0300

    Enrollment, EnrollmentPk

---
+++ Java-Spring/DsLearn/src/main/java/com/joaosilveira/dslearn/entities/Enrollment.java
@@ -0,0 +1,103 @@
+package com.joaosilveira.dslearn.entities;
+
+import com.joaosilveira.dslearn.entities.pk.EnrollmentPK;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+    private boolean avaiable;
+    private boolean onlyUpdate;
+
+    public Enrollment() {
+
+    }
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean avaiable,
+                      boolean onlyUpdate) {
+        this.id.setUser(user);
+        this.id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.avaiable = avaiable;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User student) {
+        this.id.setUser(student);
+    }
+
+    public Offer getOffer() {
+        return this.id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        this.id.setOffer(offer);
+    }
+
+
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvaiable() {
+        return avaiable;
+    }
+
+    public void setAvaiable(boolean avaiable) {
+        this.avaiable = avaiable;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enrollment that = (Enrollment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ Java-Spring/DsLearn/src/main/java/com/joaosilveira/dslearn/entities/pk/EnrollmentPK.java
@@ -0,0 +1,61 @@
+package com.joaosilveira.dslearn.entities.pk;
+
+import com.joaosilveira.dslearn.entities.Enrollment;
+import com.joaosilveira.dslearn.entities.Offer;
+import com.joaosilveira.dslearn.entities.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class EnrollmentPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrollmentPK() {
+
+    }
+
+    public EnrollmentPK(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnrollmentPK that = (EnrollmentPK) o;
+        return Objects.equals(user, that.user) && Objects.equals(offer, that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, offer);
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+}
