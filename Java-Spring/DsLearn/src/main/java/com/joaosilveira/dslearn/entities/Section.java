5d3617f36573ecce86290ab59992441fb0e69d98
Joao Henrique <joaoadsistemas@gmail.com>
2024-01-21 13:50:34 -0300

    Section

--- Java-Spring/DsLearn/src/main/java/com/joaosilveira/dslearn/entities/Section.java
+++ Java-Spring/DsLearn/src/main/java/com/joaosilveira/dslearn/entities/Section.java
@@ -1,16 +1,12 @@
 package com.joaosilveira.dslearn.entities;
 
-
-import com.joaosilveira.dslearn.enums.ResourceType;
 import jakarta.persistence.*;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource {
+@Table(name = "tb_section")
+public class Section {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,31 +17,27 @@ public class Resource {
 
     @Column(columnDefinition = "TEXT")
     private String imgUri;
-    private ResourceType type;
-
-    @Column(nullable = true)
-    private String externalLink;
 
     @ManyToOne
-    @JoinColumn(name = "offer_id")
-    private Offer offer;
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
 
-    @OneToMany(mappedBy = "resource")
-    private Set<Section> sections = new HashSet<>();
+    @ManyToOne
+    @JoinColumn(name = "prerequisite_id")
+    private Section prerequisite;
 
-    public Resource () {
+    public Section() {
 
     }
 
-    public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type, String externalLink, Offer offer) {
+    public Section(Long id, String title, String description, Integer position, String imgUri, Resource resource, Section prerequisite) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.position = position;
         this.imgUri = imgUri;
-        this.type = type;
-        this.externalLink = externalLink;
-        this.offer = offer;
+        this.resource = resource;
+        this.prerequisite = prerequisite;
     }
 
     public Long getId() {
@@ -88,40 +80,28 @@ public class Resource {
         this.imgUri = imgUri;
     }
 
-    public ResourceType getType() {
-        return type;
+    public Resource getResource() {
+        return resource;
     }
 
-    public void setType(ResourceType type) {
-        this.type = type;
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 
-    public String getExternalLink() {
-        return externalLink;
+    public Section getPrerequisite() {
+        return prerequisite;
     }
 
-    public void setExternalLink(String externalLink) {
-        this.externalLink = externalLink;
-    }
-
-    public Offer getOffer() {
-        return offer;
-    }
-
-    public void setOffer(Offer offer) {
-        this.offer = offer;
-    }
-
-    public Set<Section> getSections() {
-        return sections;
+    public void setPrerequisite(Section prerequisite) {
+        this.prerequisite = prerequisite;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Resource resource = (Resource) o;
-        return Objects.equals(id, resource.id);
+        Section section = (Section) o;
+        return Objects.equals(id, section.id);
     }
 
     @Override
