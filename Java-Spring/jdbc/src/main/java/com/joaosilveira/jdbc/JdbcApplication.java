3379af565dbd88ca63890a4ba06ddde5d2814381
Joao Henrique <joaoadsistemas@gmail.com>
2025-02-03 23:27:51 -0300

    Add JDBC support with Person entity and DAO
    Introduced `Person` entity and `PersonJdbcDAO` for database operations. Updated application to log all records from the `person` table upon startup. Enabled debug-level logging in `application.properties`.

--- Java-Spring/jdbc/src/main/java/com/joaosilveira/jdbc/JdbcApplication.java
+++ Java-Spring/jdbc/src/main/java/com/joaosilveira/jdbc/JdbcApplication.java
@@ -1,13 +1,26 @@
 package com.joaosilveira.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JdbcApplication {
+public class JdbcApplication implements CommandLineRunner {
+
+	@Autowired
+	PersonJdbcDAO personJdbcDAO;
+
+	private Logger logger = LoggerFactory.getLogger(JdbcApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All persons: {}", personJdbcDAO.findAll());
+	}
 }
---
+++ Java-Spring/jdbc/src/main/java/com/joaosilveira/jdbc/PersonJdbcDAO.java
@@ -0,0 +1,21 @@
+package com.joaosilveira.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    // select * from person
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    };
+
+}
